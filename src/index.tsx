f4fc5119c5516278dca91ae924129ae60eb51234
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-01-01 16:56:59 +0900

    설정: ...

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { client } from "./apollo";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+  darkMode: false, // or 'media' or 'class'
+};
