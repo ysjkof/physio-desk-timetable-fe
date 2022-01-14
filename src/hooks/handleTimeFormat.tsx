97095348fd279925c4b8d9ac25926f6018f71ad2
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-01-14 23:43:40 +0900

    기능추가: getYYMMDD 훅 추가
    Date에서 YYMMDD 리턴하는 함수

--- src/hooks/handleTimeFormat.tsx
+++ src/hooks/handleTimeFormat.tsx
@@ -1,9 +1,20 @@
+export const getYYMMDD = (inputDate: string | Date, option?: boolean) => {
+  const localDate = new Date(inputDate);
+  const yy = String(localDate.getFullYear()).substring(2);
+  const mm = String(localDate.getMonth() + 1).padStart(2, "0");
+  const dd = String(localDate.getDate()).padStart(2, "0");
+  if (option) {
+    return `${yy}-${mm}-${dd}`;
+  }
+  return `${yy}${mm}${dd}`;
+};
+
 export const getHHMM = (inputDate: string | Date, option?: boolean) => {
   const localDate = new Date(inputDate);
   const hh = String(localDate.getHours()).padStart(2, "0");
   const mm = String(localDate.getMinutes()).padStart(2, "0");
   if (option) {
-    return `${hh} : ${mm}`;
+    return `${hh}:${mm}`;
   }
   return `${hh}${mm}`;
 };
