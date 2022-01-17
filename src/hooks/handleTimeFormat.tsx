bdc8206a1b6def2cec88c7a68ef4837f0183c860
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-01-17 21:57:54 +0900

    기능추가: 한 주의 날짜를 배열로 얻는 훅

--- src/hooks/handleTimeFormat.tsx
+++ src/hooks/handleTimeFormat.tsx
@@ -24,3 +24,33 @@ export const getTimeLength = (startDate: Date, endDate: Date) => {
   const ed = new Date(endDate);
   return (ed.getTime() - sd.getTime()) / 1000 / 60;
 };
+
+const NUMBER_OF_A_WEEK = 7;
+export const getThisWeeksDate = () => {
+  const thisWeeks: {
+    day: number;
+    date: number;
+    month: number;
+    year: number;
+    isToday: boolean;
+    fulldate: Date;
+  }[] = [];
+  const now = new Date();
+  const nowDate = now.getDate();
+  const nowDay = now.getDay();
+  const firstDate = new Date(now.setDate(nowDate - nowDay));
+  for (let i = 0; i < NUMBER_OF_A_WEEK; i++) {
+    let date = new Date(firstDate);
+    date = new Date(date.setDate(date.getDate() + i));
+    const dateObj = {
+      day: i,
+      date: date.getDate(),
+      month: date.getMonth() + 1,
+      year: date.getFullYear(),
+      isToday: nowDate === date.getDate() && true,
+      fulldate: date,
+    };
+    thisWeeks.push(dateObj);
+  }
+  return thisWeeks;
+};
