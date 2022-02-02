c3e9e9a73d24c5967a904879ba37da3092791d08
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-02-02 21:24:57 +0900

    기능추가: NameTagSearch components
    이름, 성별, 생년월일, 등록번호를 나타내는 이름표 컴포넌트 생성

---
+++ src/components/name-tag-search.tsx
@@ -0,0 +1,75 @@
+import { makeVar, useReactiveVar } from "@apollo/client";
+import { faFemale, faMale } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getYMD } from "../hooks/handleTimeFormat";
+import { selectedPatientVar } from "../pages/reservation/reserve";
+
+interface INameTag {
+  id: number;
+  gender: string;
+  name: string;
+  registrationNumber: string | null;
+  birthday: Date;
+  canClick?: boolean;
+}
+
+export const NameTagSearch: React.FC<INameTag> = ({
+  id,
+  gender,
+  name,
+  registrationNumber,
+  birthday,
+  canClick,
+}) => {
+  const onClick = () =>
+    selectedPatientVar({
+      __typename: "Patient",
+      id,
+      gender,
+      name,
+      registrationNumber,
+      birthday,
+    });
+  return (
+    <div
+      className={`group mb-4 flex cursor-pointer items-baseline justify-between px-4 hover:bg-slate-200 ${
+        canClick ? "" : "pointer-events-none"
+      }`}
+      onClick={onClick}
+    >
+      <div className="flex items-baseline gap-2">
+        <span>
+          {gender === "male" ? (
+            <FontAwesomeIcon
+              icon={faMale}
+              className=" text-blue-500 group-hover:text-white"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faFemale}
+              className="text-pink-500 group-hover:text-white"
+            />
+          )}
+        </span>
+        <span className="dark:text-light-blue-100 font-medium text-gray-800">
+          {name}
+        </span>
+      </div>
+      <span className="dark:text-light-blue-100 text-xs text-gray-700">
+        r.no : {registrationNumber ? registrationNumber : "미등록"}
+      </span>
+      <span className="dark:text-light-blue-100 text-xs text-gray-700">
+        b : {birthday ? getYMD(birthday, "yymmdd") : "미등록"}
+      </span>
+      {/* {registrationNumber ? (
+          <span className="dark:text-light-blue-100 text-xs text-blue-600">
+            R : {registrationNumber}
+          </span>
+        ) : (
+          <span className="dark:text-light-blue-100 text-xs text-blue-600">
+            B : {getYMD(birthday, "yymmdd")}
+          </span>
+        )} */}
+    </div>
+  );
+};
