216749561f1372efe2738333869b408f8b35bc68
isj <ysjkof@gmail.com>
2022-02-10 13:56:15 +0900

    time-table 목록보기 상단에 +버튼 추가

--- src/pages/time-table.tsx
+++ src/pages/time-table.tsx
@@ -335,6 +335,20 @@ export const TimeTable = () => {
           )}
           {listView && (
             <div className="pt-1">
+              <div className="mx-auto w-fit rounded-full mt-3 mb-4 border p-1 border-dashed border-gray-500 cursor-pointer hover:bg-zinc-200 shadow">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
               {queryResult?.listReservations.results?.map((reservation) => {
                 const time =
                   getTimeLength(reservation.startDate, reservation.endDate) /
