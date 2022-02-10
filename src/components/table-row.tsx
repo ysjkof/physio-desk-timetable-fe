0754557ff74f228b63c75483c3e1fdca406cbfcc
isj <ysjkof@gmail.com>
2022-02-10 16:03:22 +0900

    styles: table-row

--- src/components/table-row.tsx
+++ src/components/table-row.tsx
@@ -23,7 +23,7 @@ export default function TableRow({
       className={cls(
         label
           ? "relative col-start-1 text-center text-sm text-gray-500 select-none"
-          : "col-start-2 border-t border-dashed text-center text-sm"
+          : "col-start-2 border-t border-dashed text-center text-sm cursor-pointer group"
       )}
       style={{ gridRowStart }}
       id={
@@ -37,10 +37,20 @@ export default function TableRow({
         <span className="relative -top-2.5 block min-h-[20px]">
           {date.getMinutes() === 0 || date.getMinutes() === 30
             ? getHHMM(date, ":")
-            : ""}
+            : null}
         </span>
       ) : (
-        <span className="block min-h-[20px]" />
+        <div className="block min-h-[20px] group-hover:shadow group-hover:rounded-lg hover:bg-zinc-2000 mx-6 group-hover:bg-gradient-to-r group-hover:from-sky-500 group-hover:to-indigo-500">
+          <span className="mx-auto w-fit hidden group-hover:block text-sm font-medium text-white">
+            {date.toLocaleString("ko-KR", {
+              month: "long",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}{" "}
+            예약하기
+          </span>
+        </div>
       )}
     </div>
   );
