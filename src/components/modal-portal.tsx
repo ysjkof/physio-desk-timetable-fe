7b607a3592b89acaf29f6651bf5a08e9b3ea84f9
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-03-22 22:06:17 +0900

    modal에서 onClick 시 닫는 기능을 처리하도록 추가

--- src/components/modal-portal.tsx
+++ src/components/modal-portal.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { createPortal } from "react-dom";
-import { useNavigate } from "react-router-dom";
 
-export const ModalPortal: React.FC = ({ children }) => {
+interface IModalPortal {
+  closeAction: any;
+}
+
+export const ModalPortal: React.FC<IModalPortal> = ({
+  closeAction,
+  children,
+}) => {
   // const container = document.getElementById("modal-root") as HTMLElement;
   const container = document.getElementById("root") as HTMLElement;
-  const navigate = useNavigate();
 
   return createPortal(
     <div className="modal fixed top-0 left-0 z-50 h-full w-full">
       <div
         className="modal-background absolute  h-full w-full bg-black opacity-50"
-        onClick={() => navigate(-1)}
+        onClick={() => closeAction()}
       />
       <div className="modal-content relative top-1/2  left-1/2 z-50 flex w-fit -translate-x-1/2 -translate-y-1/2 flex-col">
         {children}
