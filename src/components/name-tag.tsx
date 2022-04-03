2d71f21f36776cd7b64992ea42ee0b2c813a17d6
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-04-03 16:24:29 +0900

    name-tag 타입 수정

--- src/components/name-tag.tsx
+++ src/components/name-tag.tsx
@@ -1,14 +1,10 @@
 import { faFemale, faMale } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CreatePatientInput } from "../graphql/generated/graphql";
 import { cls, getYMD } from "../libs/utils";
 import { selectedPatientVar } from "../libs/variables";
 
-interface INameTag {
-  id: number;
-  gender: string;
-  name: string;
-  registrationNumber: string | null | undefined;
-  birthday: Date;
+interface INameTag extends CreatePatientInput {
   canClick?: boolean;
   shrink?: boolean;
 }
@@ -48,11 +44,11 @@ export const NameTag: React.FC<INameTag> = ({
           )}
         </span>
         <span className="dark:text-light-blue-100 min-w-[56px] overflow-hidden whitespace-nowrap text-center font-medium text-gray-800">
-          {name.length > 8 ? `${name.substring(0, 8)}...` : name}
+          {name!.length > 8 ? `${name!.substring(0, 8)}...` : name}
         </span>
       </div>
       {/* 하루 보기와 1주 보기에서 조건이 좀 다양함. 수정할 것. */}
-      {name.length > 8 ? (
+      {name!.length > 8 ? (
         ""
       ) : shrink ? (
         registrationNumber ? (
