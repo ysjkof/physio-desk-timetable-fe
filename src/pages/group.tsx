9ed5f26b6be7a5ac7d69dabdb9e94a56e4f111aa
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-04-04 18:07:59 +0900

    ... wip

--- src/pages/group.tsx
+++ src/pages/group.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { useFindGroupByIdQuery } from "../graphql/generated/graphql";
 
 export const Group = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm();
+
+  const { data, loading, error } = useFindGroupByIdQuery({
+    variables: { input: { groupId: 1 } },
+  });
+
   return (
     <>
       <Helmet>
@@ -10,13 +21,16 @@ export const Group = () => {
       </Helmet>
       <div className="container mx-auto h-full">
         <h1 className="text-2xl font-medium">그룹 목록</h1>
-        {[1, 1, 1, 1].map((group) => (
-          <>
-            <h2 className="group-name">group1</h2>
-            <h2 className="group-date">1989-11-23</h2>
-            <h2 className="group-member">1989-11-23</h2>
-          </>
-        ))}
+        <div className="flex max-w-xs flex-col items-center rounded-md border">
+          <h2 className="text-xl font-medium">
+            {data?.findGroupById.group?.name}
+          </h2>
+          <div className="flex flex-col">
+            {data?.findGroupById.group?.members.map((member) => (
+              <span>{member.member.email}</span>
+            ))}
+          </div>
+        </div>
         <h1 className="mt-16 text-2xl font-medium">그룹 만들기</h1>
       </div>
     </>
