8c98aa9ba5317da33991dfcc509b691b937caacf
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-05-05 18:44:00 +0900

    헤더에 공지 처리

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import { useReactiveVar } from "@apollo/client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { authTokenVar, isLoggedInVar } from "../apollo";
 import { LOCALSTORAGE_TOKEN } from "../variables";
@@ -9,9 +9,16 @@ import { useMe } from "../hooks/useMe";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faUser } from "@fortawesome/free-regular-svg-icons";
 
+interface Notice {
+  __typename?: "Notice" | undefined;
+  message: string;
+  read: boolean;
+}
+
 export const Header: React.FC = () => {
   // 비로그인일 때 useMe 호출되면 디버거 상태됨. 추후 처리할 것. 따로 모달창을 만드는 것 고려.
-  const { error } = useMe();
+  const { data: meData, error } = useMe();
+  const [notices, setNotices] = useState<Notice[] | null>(null);
   // const location = useLocation();
   // const state = location.state as { startDate: Date };
   const isLoggedIn = useReactiveVar(isLoggedInVar);
@@ -36,6 +43,11 @@ export const Header: React.FC = () => {
       logoutBtn();
     }
   }, [error]);
+  useEffect(() => {
+    if (meData?.me.notice) {
+      setNotices(meData.me.notice);
+    }
+  }, [meData]);
 
   // console.log("HEADER LOCATION", location);
   return (
@@ -76,10 +88,11 @@ export const Header: React.FC = () => {
                   <div className="group relative cursor-pointer">
                     <FontAwesomeIcon fontSize={"large"} icon={faBell} />
                     <div className="dropdown absolute top-6 right-0 z-50 hidden h-80 w-60 flex-col items-center overflow-y-scroll border bg-white py-2 px-4 shadow-cst group-hover:flex">
-                      {/* <span className="break-all">
-                        apple apple apple appleappleappleapple apple apple apple
-                        apple apple apple appleapple
-                      </span> */}
+                      {!notices || notices.length === 0
+                        ? "알림이 없습니다."
+                        : notices.map((notice) => (
+                            <span className="break-all">{notice.message}</span>
+                          ))}
                     </div>
                   </div>
                   <div className="group relative cursor-pointer">
