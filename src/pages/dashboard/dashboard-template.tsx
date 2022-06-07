2b81c253a7bc2d1b6b25aba29f92f6a94c240645
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-06-07 18:50:25 +0900

    대시보드 사이즈 자동조절
    #2

--- src/pages/dashboard/dashboard-template.tsx
+++ src/pages/dashboard/dashboard-template.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useRef } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface DashboardTemplateProps {
   breadcrumb: ReactNode;
@@ -11,23 +11,35 @@ export const DashboardTemplate = ({
   main,
 }: DashboardTemplateProps) => {
   const ref = useRef<HTMLDivElement>(null);
-  const headerElement = document.getElementById("header");
-  const headlessHeight = window.innerHeight - headerElement?.offsetHeight!;
-  const breadcrumbHight = ref.current?.offsetHeight!;
-  const dashboardMainHeight = headlessHeight - breadcrumbHight;
+  const [height, setHeight] = useState<number | null>(null);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | false = false;
+    function handleTableHeight() {
+      const headerElement = document.getElementById("header");
+      const headlessHeight = window.innerHeight - headerElement?.offsetHeight!;
+      const breadcrumbHight = ref.current?.offsetHeight!;
+      const dashboardMainHeight = headlessHeight - breadcrumbHight;
+
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        setHeight(dashboardMainHeight);
+      }, 200);
+    }
+    handleTableHeight();
+    window.addEventListener("resize", handleTableHeight);
+    return () => window.removeEventListener("resize", handleTableHeight);
+  }, []);
+
   return (
-    <div className="grid grid-cols-[150px,1fr] grid-rows-[2rem,1fr] ">
+    <div className="grid grid-cols-[150px,1fr] grid-rows-[2rem,1fr]">
       <div className="col-start-1 row-span-2 row-start-1">{nav}</div>
       <div className="col-start-2 border-b border-l" ref={ref}>
         {breadcrumb}
       </div>
       <div
-        className="dashboard-contents col-start-2 row-start-2 overflow-y-scroll border-l"
-        style={{
-          ...(dashboardMainHeight && {
-            height: dashboardMainHeight,
-          }),
-        }}
+        className="dashboard-contents relative col-start-2 row-start-2 overflow-y-scroll border-l"
+        style={{ ...(height && { height }) }}
       >
         {main}
       </div>
