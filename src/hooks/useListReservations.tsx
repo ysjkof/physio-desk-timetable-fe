f0f9fe1ec43ad97b85a8f7db863e13c17e08ba88
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-14 17:38:56 +0900

    refactor: useListReservations 읽기 좋게

--- src/hooks/useListReservations.tsx
+++ src/hooks/useListReservations.tsx
@@ -1,33 +1,24 @@
-import { useReactiveVar } from '@apollo/client';
-import {
-  ClinicType,
-  useListReservationsQuery,
-} from '../graphql/generated/graphql';
+import { useListReservationsQuery } from '../graphql/generated/graphql';
 import { getAfterDate, getSunday } from '../services/dateServices';
-import { clinicListsVar } from '../store';
-import { useMe } from './useMe';
 import useStore from './useStore';
 
 export const useListReservations = () => {
-  const { data } = useMe();
-  const { selectedInfo } = useStore();
-  const clinicLists = useReactiveVar(clinicListsVar);
-  const sunday = getSunday(selectedInfo.date);
+  const { selectedInfo, clinicLists } = useStore();
+
+  const startDate = getSunday(selectedInfo.date);
+  const selectedClinic = clinicLists.find(
+    (clinic) => clinic.id === selectedInfo.clinic?.id
+  );
+  const userIds = selectedClinic?.members.map((m) => m.user.id) || [0];
+  const clinicId = selectedClinic?.id || 0;
 
   return useListReservationsQuery({
     variables: {
       input: {
-        startDate: sunday,
-        endDate: getAfterDate(sunday, 7), // sunday가 1일이면 endDate는 8일 0시 00분이다. 그래서 1일~7일까지 쿼리된다.
-        userIds: clinicLists
-          .find((g) => g.id === selectedInfo.clinic?.id)
-          ?.members.map((m) => m.user.id) ?? [0],
-        clinicId:
-          selectedInfo.clinic?.id ||
-          data?.me.members!.find(
-            (member) => member.clinic.type === ClinicType.Personal
-          )?.clinic.id ||
-          0,
+        startDate,
+        endDate: getAfterDate(startDate, 7), // sunday가 1일이면 endDate는 8일 0시 00분이다. 그래서 1일~7일까지 쿼리된다.
+        userIds,
+        clinicId,
       },
     },
   });
