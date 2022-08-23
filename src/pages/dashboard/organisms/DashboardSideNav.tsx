32746b2a926956494c2f3993b3145b1c790d82e4
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-23 23:03:28 +0900

    refactor: 대시보드에 Sidebar, Selectbox 컴포넌트로 바꿈

--- src/pages/dashboard/organisms/DashboardSideNav.tsx
+++ src/pages/dashboard/organisms/DashboardSideNav.tsx
@@ -1,35 +1,31 @@
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { checkManager, checkStay, SelectedMenuType } from '..';
-import { ModalTemplate } from '../../../components/templates/ModalTemplate';
-import { MeQuery } from '../../../graphql/generated/graphql';
-import {
-  checkMember,
-  getPositionRef,
-  setLocalStorage,
-} from '../../../utils/utils';
-import { DashboardNavList } from '../components/DashboardNavList';
+import { ClinicType, MeQuery } from '../../../graphql/generated/graphql';
+import { checkMember, setLocalStorage } from '../../../utils/utils';
 import useStore from '../../../hooks/useStore';
-import { ClinicName } from '../molecules/ClinicName';
+import Sidebar from '../../../components/organisms/Sidebar';
+import Selectbox from '../../../components/organisms/Selectbox';
+import { useLocation } from 'react-router-dom';
 
+interface SelectedOption {
+  id: number;
+  value: string;
+}
 interface DashboardSideNavProps {
   selectedMenu: SelectedMenuType;
   meData: MeQuery;
 }
 
-export const DashboardSideNav = ({
-  selectedMenu,
-  meData,
-}: DashboardSideNavProps) => {
-  const { selectedInfo, clinicLists, setSelectedInfo } = useStore();
+export const DashboardSideNav = ({ meData }: DashboardSideNavProps) => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const isMatch = (endpoint: string) => pathname.includes(endpoint);
 
-  const [openClinicSelect, setOpenClinicSelect] = useState(false);
+  const { clinicLists, setSelectedInfo } = useStore();
   const onlyMeMemberClinicLists = clinicLists.map((clinic) => {
     const idx = clinic.members.findIndex(
       (member) => member.user.id === meData.me.id
     );
-
     return {
       id: clinic.id,
       name: clinic.name,
@@ -38,106 +34,103 @@ export const DashboardSideNav = ({
     };
   });
 
-  const ref = useRef<HTMLDivElement>(null);
-  const { top, left } = getPositionRef(ref, 0);
+  const [selectedOption, setSelectedOption] = useState<SelectedOption>({
+    id: onlyMeMemberClinicLists[0].id,
+    value: onlyMeMemberClinicLists[0].name,
+  });
+  const selectOption = (selectedOption: SelectedOption) => {
+    setSelectedOption(selectedOption);
+  };
+  const handleClickOption = (id: number, name: string, type: ClinicType) => {
+    selectOption({ id, value: name });
+    const newSelectedClinic = {
+      id,
+      name,
+      type,
+      members:
+        clinicLists.find((clinicInFind) => clinicInFind.id === id)?.members ??
+        [],
+      isManager: checkManager(id, meData),
+      isStayed: checkStay(id, meData),
+    };
+    setSelectedInfo('clinic', newSelectedClinic, () =>
+      setLocalStorage({
+        key: 'SELECTED_CLINIC',
+        userId: meData.me.id,
+        userName: meData.me.name,
+        value: newSelectedClinic,
+      })
+    );
+  };
 
-  return selectedInfo.clinic ? (
-    <nav className="dashboard-side-nav h-full space-y-2">
-      <div
-        className="btn-menu flex h-8 cursor-pointer items-center justify-between gap-2 rounded-none border-b px-2 py-2 text-sm font-semibold"
-        onClick={() => setOpenClinicSelect((prev) => !prev)}
-        ref={ref}
-      >
-        <span className="overflow-hidden text-ellipsis whitespace-nowrap">
-          {selectedInfo.clinic.name}
-        </span>
-        <FontAwesomeIcon icon={faChevronDown} fontSize={14} />
-        {openClinicSelect && (
-          <ModalTemplate
-            top={top}
-            left={left}
-            closeAction={() => setOpenClinicSelect}
-            children={
-              <ul className="w-[250px] divide-y overflow-hidden rounded-md bg-white text-xs font-normal shadow-cst">
-                {onlyMeMemberClinicLists.map((clinic) => (
-                  <ClinicName
-                    key={clinic.id}
-                    clinicName={clinic.name}
-                    isSelected={selectedInfo.clinic?.id === clinic.id}
-                    memberState={checkMember(
-                      clinic.member.staying,
-                      clinic.member.accepted
-                    )}
-                    onClick={() => {
-                      const newSelectedClinic = {
-                        id: clinic.id,
-                        name: clinic.name,
-                        type: clinic.type,
-                        members:
-                          clinicLists.find(
-                            (clinicInFind) => clinicInFind.id === clinic.id
-                          )?.members ?? [],
-                        isManager: checkManager(clinic.id, meData),
-                        isStayed: checkStay(clinic.id, meData),
-                      };
-                      setSelectedInfo('clinic', newSelectedClinic, () =>
-                        setLocalStorage({
-                          key: 'SELECTED_CLINIC',
-                          userId: meData.me.id,
-                          userName: meData.me.name,
-                          value: newSelectedClinic,
-                        })
-                      );
-                    }}
-                  />
-                ))}
-              </ul>
-            }
-          />
-        )}
-      </div>
-      <ul>
-        <DashboardNavList
-          type={'member'}
-          selectedClinic={selectedInfo.clinic}
-          selectedMenu={selectedMenu}
-          to="member"
-        />
-        <DashboardNavList
-          type={'invite'}
-          selectedClinic={selectedInfo.clinic}
-          selectedMenu={selectedMenu}
-          to="invite"
-        />
-        <div className="seperate-bar mb-2 pt-2" />
-        <DashboardNavList
-          type={'prescription'}
-          selectedClinic={selectedInfo.clinic}
-          selectedMenu={selectedMenu}
-          to="prescription"
-        />
-        <DashboardNavList
-          type={'statistics'}
-          selectedClinic={selectedInfo.clinic}
-          selectedMenu={selectedMenu}
-          to="statistics"
-        />
-        <div className="seperate-bar mb-2 pt-2" />
-        <DashboardNavList
-          type={'create'}
-          selectedClinic={selectedInfo.clinic}
-          selectedMenu={selectedMenu}
-          to="create"
-        />
-        <DashboardNavList
-          type={'clinics'}
-          selectedClinic={selectedInfo.clinic}
-          selectedMenu={selectedMenu}
-          to="clinics"
-        />
-      </ul>
+  return (
+    <nav className="dashboard-side-nav h-full">
+      <Selectbox selectedValue={selectedOption.value}>
+        <Selectbox.Options>
+          {onlyMeMemberClinicLists.map((clinic) => (
+            <Selectbox.Option
+              key={clinic.id}
+              selected={clinic.id === selectedOption.id}
+              onClick={() =>
+                handleClickOption(clinic.id, clinic.name, clinic.type)
+              }
+              suffix={
+                checkMember(clinic.member.staying, clinic.member.accepted) ===
+                '수락대기'
+                  ? '수락대기'
+                  : ''
+              }
+            >
+              {clinic.name}
+            </Selectbox.Option>
+          ))}
+        </Selectbox.Options>
+      </Selectbox>
+
+      <Sidebar>
+        <Sidebar.Ul>
+          {[
+            { route: 'member', name: '구성원' },
+            { route: 'invite', name: '초대' },
+          ].map((menu, idx) => (
+            <Sidebar.Li
+              key={idx}
+              to={menu.route}
+              selectedLi={isMatch(menu.route)}
+            >
+              {menu.name}
+            </Sidebar.Li>
+          ))}
+        </Sidebar.Ul>
+        <Sidebar.Ul>
+          {[
+            { route: 'prescription', name: '처방관리' },
+            { route: 'statistics', name: '통계' },
+          ].map((menu, idx) => (
+            <Sidebar.Li
+              key={idx}
+              to={menu.route}
+              selectedLi={isMatch(menu.route)}
+            >
+              {menu.name}
+            </Sidebar.Li>
+          ))}
+        </Sidebar.Ul>
+        <Sidebar.Ul>
+          {[
+            { route: 'create', name: '병원 만들기' },
+            { route: 'clinics', name: '나의 병원' },
+          ].map((menu, idx) => (
+            <Sidebar.Li
+              key={idx}
+              to={menu.route}
+              selectedLi={isMatch(menu.route)}
+            >
+              {menu.name}
+            </Sidebar.Li>
+          ))}
+        </Sidebar.Ul>
+      </Sidebar>
     </nav>
-  ) : (
-    <></>
   );
 };
