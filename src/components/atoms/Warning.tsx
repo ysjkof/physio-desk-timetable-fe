bf0d84b4b6ab2afd9fdf87703a2b1325d5dfe0fa
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-25 16:34:40 +0900

    feat: Warning 컴포넌트 수정
    - type만 아니라 자식도 받게함

--- src/components/atoms/Warning.tsx
+++ src/components/atoms/Warning.tsx
@@ -1,42 +1,37 @@
+type WarningType =
+  | 'hasNotPermission'
+  | 'hasNotStatistics'
+  | 'hasNotPrescription'
+  | 'emptyUserIds'
+  | 'emptyData'
+  | 'emptySearch'
+  | 'selectMenu'
+  | 'verifyEmail';
+
+interface ChildrenProps {
+  children: React.ReactNode;
+  type?: false;
+}
 interface WarningProps {
-  type:
-    | 'hasNotPermission'
-    | 'hasNotStatistics'
-    | 'hasNotPrescription'
-    | 'emptyUserIds'
-    | 'emptyData'
-    | 'emptySearch'
-    | 'selectMenu'
-    | 'verifyEmail';
+  type: WarningType;
+  children?: null;
 }
 
-export const Worning = ({ type }: WarningProps) => {
-  let message = '';
-  switch (type) {
-    case 'hasNotPermission':
-      message = '권한이 없습니다';
-      break;
-    case 'hasNotStatistics':
-      message = '통계 내역이 없습니다';
-      break;
-    case 'hasNotPrescription':
-      message = '처방 내역이 없습니다';
-      break;
-    case 'emptyUserIds':
-      message = '사용자를 선택해주세요';
-      break;
-    case 'emptyData':
-      message = '데이터가 없습니다';
-      break;
-    case 'emptySearch':
-      message = '검색결과가 없습니다';
-      break;
-    case 'selectMenu':
-      message = '메뉴를 선택하세요';
-      break;
-    case 'verifyEmail':
-      message = '이메일을 인증하면 모든 기능을 사용할 수 있습니다';
-      break;
-  }
-  return <h3 className="mt-10 text-center">{message}</h3>;
+export const Worning = ({ type, children }: WarningProps | ChildrenProps) => {
+  const messages = {
+    hasNotPermission: '권한이 없습니다',
+    hasNotStatistics: '통계 내역이 없습니다',
+    hasNotPrescription: '처방 내역이 없습니다',
+    emptyUserIds: '사용자를 선택해주세요',
+    emptyData: '데이터가 없습니다',
+    emptySearch: '검색결과가 없습니다',
+    selectMenu: '메뉴를 선택하세요',
+    verifyEmail: '이메일을 인증하면 모든 기능을 사용할 수있습니다',
+  };
+
+  return (
+    <p className="my-10 text-center text-base font-semibold">
+      {type ? messages[type] : children}
+    </p>
+  );
 };
