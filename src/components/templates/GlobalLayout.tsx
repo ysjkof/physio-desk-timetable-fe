290a444726757b267f85cddf05caf6e4cb81d2ee
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-08-30 15:53:02 +0900

    fix: 로그아웃일때 로그인 초기화 하지 않게 함

--- src/components/templates/GlobalLayout.tsx
+++ src/components/templates/GlobalLayout.tsx
@@ -6,9 +6,12 @@ import { Loading } from '../atoms/Loading';
 import Toast from '../molecules/Toast';
 
 export const GlobalLayout = ({ isLoggedIn }: { isLoggedIn?: boolean }) => {
-  const { loading } = useLoginInitialization();
-
+  let loading = true;
+  if (isLoggedIn) {
+    loading = useLoginInitialization().loading;
+  }
   if (isLoggedIn && loading) return <Loading></Loading>;
+
   return (
     <div className="h-screen overflow-hidden">
       {isLoggedIn ? <LoggedInGlobalNavigationBar /> : <GlobalNavigationBar />}
