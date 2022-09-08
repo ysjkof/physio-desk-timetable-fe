6077f3cfa428376cb02012888c2587c627fba296
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-08 12:43:09 +0900

    feat: 추가, viewOptions.tableDuration 변경 시 labels 다시그리기

--- src/pages/timetable/hooks/useViewOption.tsx
+++ src/pages/timetable/hooks/useViewOption.tsx
@@ -1,11 +1,10 @@
 import { useReactiveVar } from '@apollo/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TABLE_TIME_GAP } from '../../../constants/constants';
 import {
   compareDateMatch,
   get4DigitHour,
   getTimeGaps,
-  newDateFromHoursAndMinute,
   newDateSetHourAndMinute,
 } from '../../../services/dateServices';
 import { viewOptionsVar } from '../../../store';
@@ -13,44 +12,30 @@ import { viewOptionsVar } from '../../../store';
 export default function useViewoptions() {
   const viewOptions = useReactiveVar(viewOptionsVar);
 
-  const [firstTime] = useState(() => {
-    return newDateSetHourAndMinute({
-      hour: viewOptions.tableDuration.start.hours,
-      minute: viewOptions.tableDuration.start.minutes,
-    });
-  });
-  const [lastTime] = useState(() => {
-    return newDateSetHourAndMinute({
-      hour: viewOptions.tableDuration.end.hours,
-      minute: viewOptions.tableDuration.end.minutes,
-    });
-  });
+  const [firstTime] = useState(
+    newDateSetHourAndMinute({
+      hour: viewOptions.tableDuration.startHour,
+      minute: viewOptions.tableDuration.startMinute,
+    })
+  );
+  const [lastTime] = useState(
+    newDateSetHourAndMinute({
+      hour: viewOptions.tableDuration.endHour,
+      minute: viewOptions.tableDuration.endMinute,
+    })
+  );
 
   // get4DigitHour가 ISO Date String을 반환해서 로컬시각으로 표현되지 않는다
   // 그러니 화면 출력할 때 local time으로 변환해야한다
   const [labels, setLabels] = useState(
     getTimeGaps(
-      viewOptions.tableDuration.start.hours,
-      viewOptions.tableDuration.start.minutes,
-      viewOptions.tableDuration.end.hours,
-      viewOptions.tableDuration.end.minutes,
+      viewOptions.tableDuration.startHour,
+      viewOptions.tableDuration.startMinute,
+      viewOptions.tableDuration.endHour,
+      viewOptions.tableDuration.endMinute,
       TABLE_TIME_GAP
     ).map((label) => get4DigitHour(label))
   );
-
-  const isChangeOption = () => {
-    compareDateMatch(
-      newDateFromHoursAndMinute(
-        viewOptions.tableDuration.start.hours,
-        viewOptions.tableDuration.start.minutes
-      ),
-      newDateFromHoursAndMinute(
-        viewOptions.tableDuration.end.hours,
-        viewOptions.tableDuration.end.minutes
-      ),
-      'hm'
-    );
-  };
 
   const getMinute = (date: Date) => {
     return date.getTime() / 1000 / 60;
@@ -59,6 +44,37 @@ export default function useViewoptions() {
     firstTime: getMinute(firstTime),
     lastTime: getMinute(lastTime),
   };
+
+  useEffect(() => {
+    const isMatchFirstTime = compareDateMatch(
+      firstTime,
+      newDateSetHourAndMinute({
+        hour: viewOptions.tableDuration.startHour,
+        minute: viewOptions.tableDuration.startMinute,
+      }),
+      'hm'
+    );
+    const isMatchLastime = compareDateMatch(
+      lastTime,
+      newDateSetHourAndMinute({
+        hour: viewOptions.tableDuration.endHour,
+        minute: viewOptions.tableDuration.endMinute,
+      }),
+      'hm'
+    );
+
+    if (!isMatchFirstTime || !isMatchLastime) {
+      setLabels(
+        getTimeGaps(
+          viewOptions.tableDuration.startHour,
+          viewOptions.tableDuration.startMinute,
+          viewOptions.tableDuration.endHour,
+          viewOptions.tableDuration.endMinute,
+          TABLE_TIME_GAP
+        ).map((label) => get4DigitHour(label))
+      );
+    }
+  }, [viewOptions]);
 
   return { labels, setLabels, indicatorTimes };
 }
