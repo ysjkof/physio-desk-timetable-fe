a955ff3378e7f0c94706b32781126856631add80
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-10 16:24:36 +0900

    refactor: 대시보드 멤버에 카드 컴포넌트 적용

--- src/pages/dashboard/components/organisms/Members.tsx
+++ src/pages/dashboard/components/organisms/Members.tsx
@@ -1,9 +1,9 @@
-import { checkManager, cls } from '../../../../utils/utils';
-import { DashboardSectionLayout } from '../template/DashboardSectionLayout';
+import { checkManager, getMemberState } from '../../../../utils/utils';
 import useStore from '../../../../hooks/useStore';
 import { Button } from '../../../../components/molecules/Button';
 import useCancelInvitation from '../../hooks/useCancelInvitation';
 import { loggedInUserVar } from '../../../../store';
+import Card from '../molecules/Card';
 
 export const Members = () => {
   const loggedInUser = loggedInUserVar();
@@ -11,68 +11,43 @@ export const Members = () => {
   const { invokeCancelInvitation, loading: loadingCancel } =
     useCancelInvitation();
 
+  const isManager = checkManager(
+    selectedInfo.clinic?.members!,
+    loggedInUser!.id
+  );
+
   return (
-    <DashboardSectionLayout
-      title="구성원"
-      heightFull
-      width="md"
-      children={
-        <>
-          <div className="grid grid-cols-[2.4rem_1fr_4rem_4rem] gap-3 border-b">
-            <span className="">권한</span>
-            <span className="">이름</span>
-            <span className=""></span>
-            <span className="text-center">상태</span>
-          </div>
-          <ul className="space-y-2">
-            {selectedInfo.clinic?.members?.map((member) => (
-              <div
-                key={member.id}
-                className="grid grid-cols-[2.4rem_1fr_4rem_4rem] items-center gap-3 "
-              >
-                <span className="inline-block w-10 ">
-                  {member.manager ? '관리자' : '회원'}
-                </span>
-                <span
-                  className={cls(
-                    'py-1',
-                    loggedInUser?.id === member.user.id ? 'font-bold' : ''
-                  )}
-                >
-                  {member.user.name}
-                </span>
-                {!member.staying && !member.accepted && (
-                  <>
-                    {checkManager(
-                      selectedInfo.clinic?.members!,
-                      loggedInUser!.id
-                    ) ? (
-                      <Button
-                        type="button"
-                        loading={loadingCancel}
-                        canClick={!loadingCancel}
-                        isSmall
-                        onClick={() => invokeCancelInvitation(member.id, true)}
-                      >
-                        초대 취소
-                      </Button>
-                    ) : (
-                      <span />
-                    )}
-                    <span className="text-center text-red-500">승인대기</span>
-                  </>
-                )}
-                {!member.staying && member.accepted && (
-                  <>
-                    <span />
-                    <span className="text-center text-red-500">떠난 회원</span>
-                  </>
-                )}
-              </div>
-            ))}
-          </ul>
-        </>
-      }
-    />
+    <section className="h-full">
+      <Card.Container>
+        {selectedInfo.clinic?.members?.map((member) => {
+          const state = getMemberState(
+            member.staying,
+            member.accepted,
+            member.manager
+          );
+          return (
+            <Card
+              key={member.user.id}
+              name={member.user.name}
+              state={state}
+              button={
+                state === '승인대기' &&
+                isManager && (
+                  <Button
+                    type="button"
+                    isSmall
+                    canClick={!loadingCancel}
+                    loading={loadingCancel}
+                    onClick={() => invokeCancelInvitation(member.id, true)}
+                  >
+                    초대취소
+                  </Button>
+                )
+              }
+            />
+          );
+        })}
+      </Card.Container>
+    </section>
   );
 };
