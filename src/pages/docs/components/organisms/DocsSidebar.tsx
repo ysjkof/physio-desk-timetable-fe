be035eb71288a20c5e511a84c3360390c9525192
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-12 23:10:06 +0900

    refactor: DocsSidebar.tsx 선택한 메뉴 눈에 띄게함

--- src/pages/docs/components/organisms/DocsSidebar.tsx
+++ src/pages/docs/components/organisms/DocsSidebar.tsx
@@ -1,14 +1,24 @@
+import { useLocation } from 'react-router-dom';
 import Sidebar from '../../../../components/organisms/Sidebar';
 import { docsMenu } from '../../../../router/routes';
 
 export default function DocsSidebar() {
+  const location = useLocation();
+  console.log(location);
+  const splitted = location.pathname.split('/');
+  const endpoint = splitted[splitted.length - 1];
+
   return (
     <aside className="w-full max-w-xs border-r">
       <Sidebar>
         {docsMenu.map((menu) => (
           <Sidebar.Ul key={menu.name} title={menu.name}>
             {menu.children.map((submenu) => (
-              <Sidebar.Li key={submenu.name} to={menu.route + submenu.route}>
+              <Sidebar.Li
+                key={submenu.name}
+                to={menu.route + submenu.route}
+                selected={endpoint === submenu.route}
+              >
                 {submenu.name}
               </Sidebar.Li>
             ))}
