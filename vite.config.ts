d08e1a43bca169bed41355713380b28ae4b5d3df
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-12 21:07:30 +0900

    chore: mdx-js 설정, tailwind typography plugin 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react(), visualizer(), mdx({ remarkPlugins: [remarkGfm] })],
 });
