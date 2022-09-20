feb706b3e9ff0cba3736c88cc8fb95ed082e8c94
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-20 14:52:08 +0900

    refactor: 상태 배지 css로 분리

--- src/components/atoms/StateBadge.tsx
+++ src/components/atoms/StateBadge.tsx
@@ -1,24 +1,31 @@
 import { HTMLAttributes } from 'react';
+import { ChildrenProps } from '../../types/type';
 import { cls, StayingState } from '../../utils/utils';
 
-interface StateBadgeProps extends HTMLAttributes<HTMLSpanElement> {
-  state: StayingState;
+interface StateBadgeProps
+  extends HTMLAttributes<HTMLSpanElement>,
+    ChildrenProps {
+  state?: StayingState;
 }
-export default function StateBadge({ state, ...args }: StateBadgeProps) {
+export default function StateBadge({
+  state,
+  children,
+  ...args
+}: StateBadgeProps) {
   return (
     <span
       {...args}
       className={cls(
-        'ml-2 w-fit whitespace-nowrap',
+        'w-fit whitespace-nowrap',
         state === '탈퇴'
-          ? 'rounded-md bg-gray-100 px-1.5 py-0.5 text-gray-700'
+          ? 'badge-gray'
           : state === '승인대기'
-          ? 'rounded-md bg-red-100 px-1.5 py-0.5 text-red-700'
-          : 'rounded-md bg-green-100 px-1.5 py-0.5 text-green-700',
+          ? 'badge-red '
+          : 'badge-green',
         args.className || ''
       )}
     >
-      {state}
+      {state || children}
     </span>
   );
 }
