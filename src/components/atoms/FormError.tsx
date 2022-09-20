47b879b3d3b544c9d39985ad1f9e928e7bf55942
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-20 21:32:10 +0900

    refactor: FormError에 ClassNameProps 추가

--- src/components/atoms/FormError.tsx
+++ src/components/atoms/FormError.tsx
@@ -1,13 +1,24 @@
+import { ClassNameProps } from '../../types/type';
 import { cls } from '../../utils/utils';
 
-interface FormErrorProps {
+interface FormErrorProps extends ClassNameProps {
   errorMessage: string;
   isHighter?: boolean;
 }
 
-export default function FormError({ errorMessage, isHighter }: FormErrorProps) {
+export default function FormError({
+  errorMessage,
+  isHighter,
+  className,
+}: FormErrorProps) {
   return (
-    <span className={cls('form-error', isHighter ? '-top-7' : '-top-1')}>
+    <span
+      className={cls(
+        'absolute right-0 whitespace-nowrap rounded-md px-2 py-1 font-semibold text-red-400',
+        isHighter ? '-top-7' : '-top-1',
+        className || ''
+      )}
+    >
       {errorMessage}
     </span>
   );
