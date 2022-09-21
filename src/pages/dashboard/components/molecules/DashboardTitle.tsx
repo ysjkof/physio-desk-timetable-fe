8d933e1debedd26ed3d4963e32ac655cc5c44cbb
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-21 14:19:26 +0900

    fix: 대시보드 타이틀에서 병원 이름 제거

--- src/pages/dashboard/components/molecules/DashboardTitle.tsx
+++ src/pages/dashboard/components/molecules/DashboardTitle.tsx
@@ -2,19 +2,13 @@ import { DASHBOARD_MENU_KR } from '../../../../constants/constants';
 import { DashboardEndpoint } from '../../../../router/routes';
 
 interface DashboardTitleProps {
-  clinicName: string;
   endpoint: DashboardEndpoint;
 }
 
-export default function DashboardTitle({
-  clinicName,
-  endpoint,
-}: DashboardTitleProps) {
+export default function DashboardTitle({ endpoint }: DashboardTitleProps) {
   return (
     <h2 className="bg-white">
-      <span>
-        {clinicName} &gt; {DASHBOARD_MENU_KR[endpoint]}
-      </span>
+      <span>{DASHBOARD_MENU_KR[endpoint]}</span>
     </h2>
   );
 }
