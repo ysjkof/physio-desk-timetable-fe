a2136988c6dd7efc894cfb45e3dbf68bf7533fcd
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-09-25 13:03:08 +0900

    chore: Calendar SVG icon 추가

---
+++ src/svgs/Calendar.tsx
@@ -0,0 +1,22 @@
+import { SVG } from '../types/type';
+import { cls } from '../utils/utils';
+
+export default function Calendar({ ...args }: SVG) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+      {...args}
+      className={cls('h-6 w-6', args.className || '')}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+      />
+    </svg>
+  );
+}
