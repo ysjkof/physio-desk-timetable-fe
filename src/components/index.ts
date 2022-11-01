f16d869a46ae631392e0790cc9c4a67df1cf365c
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-01 21:27:58 +0900

    chore: svgs, coponents가 index.ts에서 export함

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { CheckableButton } from './CheckableButton';
+import { MenuButton } from './MenuButton';
+import { TwoLabelSwitch } from './TwoLabelSwitch';
+
+export { CheckableButton, MenuButton, TwoLabelSwitch };
