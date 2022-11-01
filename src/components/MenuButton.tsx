d32539083fa6163414aad79c38487ec02b98b05f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-01 15:17:40 +0900

    UI: MenuButton에 fowardRef로 포장함

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import { cls } from '../utils/utils';
 
 interface MenuButtonProps {
@@ -8,30 +9,33 @@ interface MenuButtonProps {
   label: string;
   onClick: () => void;
 }
-export function MenuButton({
-  icon,
-  isActivated,
-  color,
-  backgroundColor,
-  label,
-  onClick,
-}: MenuButtonProps) {
-  return (
-    <div
-      className={cls(
-        'flex h-8 w-fit items-center gap-2 rounded-sm border border-gray-500 px-2',
-        isActivated ? 'bg-gray-500 text-white' : ''
-      )}
-      onClick={onClick}
-      style={{
-        ...(isActivated && {
-          backgroundColor,
-          color,
-        }),
-      }}
-    >
-      {icon && icon}
-      {label}
-    </div>
-  );
-}
+export const MenuButton = forwardRef<HTMLButtonElement, MenuButtonProps>(
+  function MenuButton({
+    icon,
+    isActivated,
+    color,
+    backgroundColor,
+    label,
+    onClick,
+  }: MenuButtonProps) {
+    return (
+      <div
+        className={cls(
+          'flex h-8 w-fit select-none items-center gap-2 whitespace-nowrap rounded-sm border border-gray-500 px-2',
+          isActivated ? 'bg-gray-500 text-white' : ''
+        )}
+        onClick={onClick}
+        style={{
+          ...(backgroundColor && {
+            backgroundColor,
+            borderColor: backgroundColor,
+            color,
+          }),
+        }}
+      >
+        {icon && icon}
+        {label}
+      </div>
+    );
+  }
+);
