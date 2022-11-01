19f24533bde3800a52b9708bf4838b38f875ff16
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-01 17:16:31 +0900

    chore: SVG icon 컴포넌트 추가(ChevronLeft, Right, EllipsisVertical, UserPlus)

--- src/svgs/UserPlus.tsx
+++ src/svgs/UserPlus.tsx
@@ -3,14 +3,16 @@ import { cls } from '../utils/utils';
 
 /**
  * heroicicons.com
- * Check > mini
+ * UserPlus > Outline
  */
-export default function Check({ ...args }: SVG) {
+export default function UserPlus({ ...args }: SVG) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
       {...args}
       className={cls(
         args.className || '',
@@ -24,9 +26,9 @@ export default function Check({ ...args }: SVG) {
       )}
     >
       <path
-        fillRule="evenodd"
-        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-        clipRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
       />
     </svg>
   );
