30c8023240230acdd7154b42ad4858fbe52e5cf1
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-07 22:15:52 +0900

    chore: 테일윈드 띰에 table-aside 배경색 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,6 +21,7 @@ module.exports = {
       colors: {
         'table-bg': '#E4E4ED',
         'table-line': '#D8D8E9',
+        'table-aside-bg': '#262850',
       },
       backgroundImage: {
         'check-white': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='4' stroke='white' class='w-6 h-6'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' d='M4.5 12.75l6 6 9-13.5' /%3E%3C/svg%3E")`,
