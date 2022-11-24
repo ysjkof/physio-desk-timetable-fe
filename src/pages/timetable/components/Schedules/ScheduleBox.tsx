73904b7808ba55af2c2530ebf9f2e20182a5ccb1
seongjin <ysjkof@gmail.com>
2022-11-24 16:18:30 +0900

    style: 형제 컴포넌트와 반환 형식 통일

--- src/pages/timetable/components/Schedules/ScheduleBox.tsx
+++ src/pages/timetable/components/Schedules/ScheduleBox.tsx
@@ -1,8 +1,8 @@
-import { cls } from "../../../../utils/utils";
-import TimeIndicatorBar from "../../_legacy_components/organisms/TimeIndicatorBar";
-import ReservationButtons from "./ReserveButtons";
-import EventBoxContainer from "./EventBoxContainer";
-import type { ScheduleBoxProps } from "../../../../types/props.types";
+import { cls } from '../../../../utils/utils';
+import TimeIndicatorBar from '../../_legacy_components/organisms/TimeIndicatorBar';
+import ReservationButtons from './ReserveButtons';
+import EventBoxContainer from './EventBoxContainer';
+import type { ScheduleBoxProps } from '../../../../types/props.types';
 
 function ScheduleBox({
   userLength,
@@ -16,35 +16,33 @@ function ScheduleBox({
   return (
     <div
       className={cls(
-        "USER_COLS relative grid gap-2",
-        userLength === 1 ? "border-x-inherit" : ""
+        'USER_COLS relative grid gap-2',
+        userLength === 1 ? 'border-x-inherit' : ''
       )}
       style={viewPeriodStyle}
     >
       <TimeIndicatorBar isActive={enableTimeIndicator} />
-      {users.map((member, userIndex) => {
-        return (
-          <div
-            key={member.id}
-            className="USER_COL relative w-full divide-y divide-table-line bg-table-bg hover:bg-gray-200/50"
-          >
-            <ReservationButtons
-              labelMaxLength={labelMaxLength}
-              date={date}
-              labels={labels}
-              userId={member.user.id}
-              userIndex={userIndex}
-            />
-            <EventBoxContainer
-              labelMaxLength={labelMaxLength}
-              labels={labels}
-              events={member.events}
-              userIndex={userIndex}
-              isSingleUser={userLength === 1}
-            />
-          </div>
-        );
-      })}
+      {users.map((member, userIndex) => (
+        <div
+          key={member.id}
+          className="USER_COL relative w-full divide-y divide-table-line bg-table-bg hover:bg-gray-200/50"
+        >
+          <ReservationButtons
+            labelMaxLength={labelMaxLength}
+            date={date}
+            labels={labels}
+            userId={member.user.id}
+            userIndex={userIndex}
+          />
+          <EventBoxContainer
+            labelMaxLength={labelMaxLength}
+            labels={labels}
+            events={member.events}
+            userIndex={userIndex}
+            isSingleUser={userLength === 1}
+          />
+        </div>
+      ))}
     </div>
   );
 }
