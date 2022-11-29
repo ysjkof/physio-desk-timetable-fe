4d13740292da59b165aa7260099dacf00235ca97
seongjin <ysjkof@gmail.com>
2022-11-29 16:30:30 +0900

    refactor: useTimetable -> useSchedules 이름 변경

--- src/pages/timetable/hooks/index.ts
+++ src/pages/timetable/hooks/index.ts
@@ -2,7 +2,7 @@ import { useDayoff } from './useDayoff';
 import { useDeleteReservation } from './useDeleteReservation';
 import { useListReservations } from './useListReservations';
 import { useReserve } from './useReserve';
-import { useSchedules } from './useTimetable';
+import { useSchedules } from './useSchedules';
 import { useSelectedClinic } from './useSelectedClinic';
 import { useTableDisplay } from './useTableDisplay';
 import { useTableLabel } from './useTableLabel';
