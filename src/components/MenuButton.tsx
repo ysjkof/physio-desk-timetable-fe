a17c697ed955ffbdf1826958a8be10fb1e549053
seongjin <ysjkof@gmail.com>
2022-12-11 21:53:00 +0900

    feat: MenuButton 컴포넌트에 borderNone props 추가

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -5,15 +5,20 @@ interface MenuButtonProps extends PropsWithChildren {
   isActivated?: boolean;
   color?: string;
   backgroundColor?: string;
+  borderNone?: boolean;
   onClick: () => void;
 }
 
 const MenuButton = forwardRef<HTMLButtonElement, MenuButtonProps>(
-  ({ isActivated, color, backgroundColor, children, onClick }, ref) => (
+  (
+    { isActivated, color, backgroundColor, borderNone, children, onClick },
+    ref
+  ) => (
     <button
       className={cls(
-        'flex h-8 w-fit select-none items-center gap-2 whitespace-nowrap rounded-sm border border-gray-500 px-2',
-        isActivated ? 'bg-gray-500 text-white' : ''
+        'flex h-8 w-fit select-none items-center gap-2 whitespace-nowrap rounded-sm px-2',
+        isActivated ? 'bg-gray-500 text-white' : '',
+        borderNone ? '' : 'border border-gray-500'
       )}
       onClick={onClick}
       style={{
