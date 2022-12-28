ddbbc288bfe2c06c41dc1ab908d1bd5d1b153fa0
seongjin <ysjkof@gmail.com>
2022-12-28 20:41:57 +0900

    fix: Dayoff 위치 제대로 못 잡는 문제 해결

--- src/pages/timetable/components/Schedules/EventBoxContainer.tsx
+++ src/pages/timetable/components/Schedules/EventBoxContainer.tsx
@@ -1,12 +1,7 @@
 import { memo } from 'react';
-import {
-  createDate,
-  get4DigitHour,
-  getTimeLength,
-} from '../../../../utils/date.utils';
+import { get4DigitHour, getTimeLength } from '../../../../utils/date.utils';
 import EventBox from './EventBox';
 import { TABLE_CELL_HEIGHT } from '../../../../constants/constants';
-import { ReservationState } from '../../../../types/generated.types';
 import type { ReservationInList } from '../../../../types/common.types';
 
 interface EventBoxContainerProps {
@@ -26,44 +21,27 @@ const EventBoxContainer = ({
 }: EventBoxContainerProps) => {
   const maxTableHeight = labelMaxLength * TABLE_CELL_HEIGHT - TABLE_CELL_HEIGHT;
 
-  const [hour, minute] = labels[0].split(':');
   const endLabelIdx = labels.length - 1;
-
-  const firstLabelDate = createDate(events[0]?.startDate, {
-    hour: +hour,
-    minute: +minute,
-  });
 
   return (
     <>
       {events.map((event) => {
         let inset = '0';
-        let numberOfCell = getTimeLength(
+        const numberOfCell = getTimeLength(
           event.startDate,
           event.endDate,
           '20minute'
         );
+        const labelIdx = labels.findIndex((label) => {
+          return label === get4DigitHour(event.startDate);
+        });
+        /**
+         * 시작시간이 마지막 시작 시간과 같으면 화면을 벗어나기 때문에 리턴
+         * labelIdx가 없으면 화면을 벗어나기 때문에 한 번 더 확인하고 리턴
+         */
+        if (labelIdx === endLabelIdx || labelIdx === -1) return false;
 
-        if (event.state === ReservationState.DayOff) {
-          numberOfCell =
-            numberOfCell +
-            getTimeLength(
-              firstLabelDate,
-              new Date(event.startDate),
-              '20minute'
-            );
-        } else {
-          const labelIdx = labels.findIndex((label) => {
-            return label === get4DigitHour(event.startDate);
-          });
-          /**
-           * 시작시간이 마지막 시작 시간과 같으면 화면을 벗어나기 때문에 리턴
-           * labelIdx가 없으면 화면을 벗어나기 때문에 한 번 더 확인하고 리턴
-           */
-          if (labelIdx === endLabelIdx || labelIdx === -1) return false;
-
-          inset = `${labelIdx * TABLE_CELL_HEIGHT}px 0`;
-        }
+        inset = `${labelIdx * TABLE_CELL_HEIGHT}px 0`;
 
         return (
           <EventBox
