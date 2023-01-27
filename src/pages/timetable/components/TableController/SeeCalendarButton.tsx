5c2357d46af2d512fdd84b175874e164ba28ee48
seongjin <ysjkof@gmail.com>
2023-01-27 19:25:37 +0900

    feat: SeeCalendarButton 기능 구현(캘린더 표시)
    전에는 기능없이 버튼만 있었는데 기능 구현함

--- src/pages/timetable/components/TableController/SeeCalendarButton.tsx
+++ src/pages/timetable/components/TableController/SeeCalendarButton.tsx
@@ -1,6 +1,9 @@
+import { useRef } from 'react';
 import { Calendar } from '../../../../svgs';
-import { MenuButton } from '../../../../components';
+import { Datepicker, MenuButton, Modal } from '../../../../components';
 import { useTableDisplay } from '../../hooks';
+import { getPositionRef } from '../../../../utils/common.utils';
+import { selectedDateVar } from '../../../../store';
 
 const SeeCalendarButton = () => {
   const { tableDisplay, toggleDisplayOption } = useTableDisplay();
@@ -8,11 +11,30 @@ const SeeCalendarButton = () => {
     toggleDisplayOption('seeCalendar');
   };
 
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const { top } = getPositionRef(buttonRef);
+
+  const setDate = (date: Date) => {
+    selectedDateVar(date);
+  };
+
   return (
-    <MenuButton onClick={toggleCalender} isActivated={tableDisplay.seeCalendar}>
-      <Calendar />
-      달력보기
-    </MenuButton>
+    <>
+      <MenuButton
+        onClick={toggleCalender}
+        isActivated={tableDisplay.seeCalendar}
+        ref={buttonRef}
+        hasBorder
+      >
+        <Calendar />
+        달력보기
+      </MenuButton>
+      {tableDisplay.seeCalendar && (
+        <Modal top={top} right={10} closeAction={toggleCalender}>
+          <Datepicker closeAction={toggleCalender} setDate={setDate} />
+        </Modal>
+      )}
+    </>
   );
 };
 
