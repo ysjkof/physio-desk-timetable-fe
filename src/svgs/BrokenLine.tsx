988004e69b4aa8127ecaac7e29ea48a6a8f3afff
seongjin <ysjkof@gmail.com>
2023-01-28 14:56:12 +0900

    chore: SVG 구현(BrokenLine, Heart, Medicine)

---
+++ src/svgs/BrokenLine.tsx
@@ -0,0 +1,34 @@
+import { SVG } from '../types/common.types';
+import { cls } from '../utils/common.utils';
+
+/**
+ * by JH
+ */
+const BrokenLine = ({ iconSize = 'MD', ...args }: SVG) => {
+  return (
+    <svg
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...args}
+      className={cls(
+        args.className || '',
+        iconSize === 'LG' ? 'h-6 w-6' : '',
+        iconSize === 'MD' ? 'h-4 w-4' : '',
+        iconSize === 'SM' ? 'h-3 w-3' : ''
+      )}
+    >
+      <path
+        d="M1 10L3.5 4.5L7.5 11L12.5 1.5L15 7"
+        stroke="#8D8DAD"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default BrokenLine;
--- src/svgs/index.ts
+++ src/svgs/index.ts
@@ -1,4 +1,5 @@
 import BarBottomLeft from './BarBottomLeft';
+import BrokenLine from './BrokenLine';
 import Building from './Building';
 import Calendar from './Calendar';
 import Check from './Check';
@@ -9,6 +10,8 @@ import ChevronUp from './ChevronUp';
 import CogSixTooth from './CogSixTooth';
 import Edit from './Edit';
 import EllipsisVertical from './EllipsisVertical';
+import Heart from './Heart';
+import Medicine from './Medicine';
 import Table from './Table';
 import Trash from './Trash';
 import UserPlus from './UserPlus';
@@ -16,6 +19,7 @@ import XMark from './XMark';
 
 export {
   BarBottomLeft,
+  BrokenLine,
   Building,
   Calendar,
   Check,
@@ -26,6 +30,8 @@ export {
   CogSixTooth,
   Edit,
   EllipsisVertical,
+  Heart,
+  Medicine,
   Table,
   Trash,
   UserPlus,
