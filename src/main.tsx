a06ef6e020914a1f4a13766ea9f33484e2d4e3aa
seongjin <ysjkof@gmail.com>
2023-01-29 12:05:57 +0900

    chore: custom.css를 tailwind.css에 합침

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import router from './router/router';
-import './styles/custom.css';
 import './styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
