4407fa1665905541bde07021942aa02050c4e8df
seongjin <ysjkof@gmail.com>
2023-01-29 16:49:42 +0900

    feat: MenuContainer LinkButton 분리하고 활성시 강조 효과 적용

--- src/pages/dashboard/components/MenuContainer/MenuContainer.tsx
+++ src/pages/dashboard/components/MenuContainer/MenuContainer.tsx
@@ -1,33 +1,56 @@
+import { type PropsWithChildren } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { BrokenLine, Heart, Medicine } from '../../../../svgs';
+import { cls } from '../../../../utils/common.utils';
 
 const MenuContainer = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className="flex flex-col text-sm">
-      <button
-        className="flex items-center gap-2 py-2 text-table-day-strong"
-        onClick={() => {}}
-        type="button"
+      <LinkButton
+        path="/dashboard/clinic/members"
+        isActivate={pathname === '/dashboard/clinic/members'}
       >
         <Heart />
         직원열람 및 관리
-      </button>
-      <button
-        className="flex items-center gap-2 py-2 text-table-day-strong"
-        onClick={() => {}}
-        type="button"
+      </LinkButton>
+      <LinkButton
+        path="/dashboard/clinic/prescriptions"
+        isActivate={pathname === '/dashboard/clinic/prescriptions'}
       >
         <Medicine />
         처방등록 및 관리
-      </button>
-      <button
-        className="flex items-center gap-2 py-2 text-table-day-strong"
-        onClick={() => {}}
-        type="button"
+      </LinkButton>
+      <LinkButton
+        path="/dashboard/clinic/statistics"
+        isActivate={pathname === '/dashboard/clinic/statistics'}
       >
         <BrokenLine />
         통계
-      </button>
+      </LinkButton>
     </div>
+  );
+};
+
+interface LinkButtonProps extends PropsWithChildren {
+  isActivate: boolean;
+  path: string;
+}
+
+const LinkButton = ({ children, path, isActivate }: LinkButtonProps) => {
+  return (
+    <Link
+      to={path}
+      className={cls(
+        'flex items-center gap-2 rounded-md py-2.5 px-2 pl-4 font-bold',
+        isActivate
+          ? 'bg-[#EEEEFF] text-table-aside-bg'
+          : 'text-table-day-strong'
+      )}
+    >
+      {children}
+    </Link>
   );
 };
 
