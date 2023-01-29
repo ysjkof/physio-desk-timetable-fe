d1d3aeba8dc1a62d3c2a42586f5c9376765b6aa5
seongjin <ysjkof@gmail.com>
2023-01-29 17:09:45 +0900

    feat: 대시보드에 MemberListItem 클릭시 member.id로 링크 이동

--- src/pages/dashboard/components/MemberList/MemberListItem.tsx
+++ src/pages/dashboard/components/MemberList/MemberListItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { USER_COLORS } from '../../../../constants/constants';
 import { cls, getMemberState } from '../../../../utils/common.utils';
 import type { MemberOfClient } from '../../../../types/common.types';
@@ -15,25 +16,31 @@ const MemberListItem = ({ member, userIndex }: MemberListItemProps) => {
   });
 
   return (
-    <li
-      key={member.id}
-      className="flex items-center justify-between gap-4 py-4 px-4 text-base"
-    >
-      <span
-        className="aspect-square w-9 rounded-md bg-red-200 text-center text-white"
-        style={{ backgroundColor: USER_COLORS[userIndex].deep }}
+    <li key={member.id}>
+      <Link
+        to={`${member.id}`}
+        className="flex items-center justify-between gap-4 py-4 px-4 text-base"
       >
-        {member.user.name.substring(0, 1)}
-      </span>
-      <span className="basis-full text-table-aside-bg">{member.user.name}</span>
-      <span
-        className={cls(
-          'w-24',
-          memberState === '승인대기' ? 'text-caution' : 'text-table-day-strong'
-        )}
-      >
-        {memberState}
-      </span>
+        <span
+          className="aspect-square w-9 rounded-md bg-red-200 text-center text-white"
+          style={{ backgroundColor: USER_COLORS[userIndex].deep }}
+        >
+          {member.user.name.substring(0, 1)}
+        </span>
+        <span className="basis-full text-table-aside-bg">
+          {member.user.name}
+        </span>
+        <span
+          className={cls(
+            'w-24',
+            memberState === '승인대기'
+              ? 'text-caution'
+              : 'text-table-day-strong'
+          )}
+        >
+          {memberState}
+        </span>
+      </Link>
     </li>
   );
 };
