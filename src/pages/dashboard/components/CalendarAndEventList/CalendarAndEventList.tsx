11f999da507b71c1fff76ad4b8163d088602040e
seongjin <ysjkof@gmail.com>
2023-02-02 12:34:02 +0900

    chore: CalendarAndEventList CSS 수정
    container에 justify-between 추가

--- src/pages/dashboard/components/CalendarAndEventList/CalendarAndEventList.tsx
+++ src/pages/dashboard/components/CalendarAndEventList/CalendarAndEventList.tsx
@@ -5,10 +5,8 @@ import { EventList } from '../EventList';
 const CalendarAndEventList = () => {
   const [date, setDate] = useState(new Date());
 
-  // TODO: EventList 개선
-
   return (
-    <div className="mr-10 flex flex-col gap-y-6 py-10">
+    <div className="mr-10 flex flex-col justify-between gap-y-6 py-10">
       <Datepicker selectedDate={date} selectDate={setDate} />
       <EventList date={date} />
     </div>
