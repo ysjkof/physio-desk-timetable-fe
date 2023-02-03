d2ec94204c023201aff17b59b078ea97dc66ffad
seongjin <ysjkof@gmail.com>
2023-02-03 13:47:58 +0900

    feat: printGraphQLErrors 구현, printNetworkError가 시스템 메시지 출력하지 않게함

--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -1,14 +1,21 @@
 import { toastVar } from '../store';
 
-export function printNetworkError(message: string, route?: string) {
-  const setMessage = (errMessage: string) => {
-    const returnMessage = [
-      '요청을 실패했습니다. 서버가 정지했을 수 있습니다.',
-      '문제가 계속되면 고객센터에 문의하세요',
-      errMessage || '',
-    ];
-    if (route) returnMessage.push(`문제가 발생한 곳 : ${route}`);
-    return returnMessage;
-  };
-  toastVar({ messages: setMessage(message) });
+export function printNetworkError(route?: string) {
+  const messages: string[] = [
+    '요청을 실패했습니다.',
+    '이 문제는 브라우저 새로고침으로 해결될 수 있습니다.',
+    '문제가 계속되면 고객센터에 문의하세요',
+  ];
+
+  if (route) messages.push(`문제가 발생한 곳 : ${route}`);
+
+  toastVar({ messages });
+}
+
+const GQL_ERROR = {
+  gotInvalidValue: 'input에 잘못된 값을 받았습니다.',
+} as const;
+
+export function printGraphQLErrors(error: keyof typeof GQL_ERROR) {
+  toastVar({ messages: [GQL_ERROR[error]] });
 }
