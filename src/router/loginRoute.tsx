d5bcf1ca9726fbfea331b07db1f1d21ce5e71e60
seongjin <ysjkof@gmail.com>
2023-02-06 18:17:16 +0900

    chore: 레거시 대시보드 라우터 주석처리

--- src/router/loginRoute.tsx
+++ src/router/loginRoute.tsx
@@ -1,6 +1,5 @@
 import { lazy } from 'react';
 import { ROUTES } from './routes';
-import CreatePrescription from '../pages/legacy_dashboard/components/organisms/CreatePrescription';
 import {
   MemberDetail,
   MemberManagement,
@@ -13,28 +12,28 @@ import { MyProfile } from '../pages/setting/components';
 const ProtectRoute = lazy(() => import('./ProtectRoute'));
 const TimeTable = lazy(() => import('../pages/timetable/Timetable'));
 const Dashboard = lazy(() => import('../pages/dashboard/Dashboard'));
-const CreateClinic = lazy(
-  () => import('../pages/legacy_dashboard/components/organisms/CreateClinic')
-);
-const InviteClinic = lazy(
-  () => import('../pages/legacy_dashboard/components/organisms/InviteClinic')
-);
-const Members = lazy(
-  () => import('../pages/legacy_dashboard/components/organisms/Members')
-);
-const MyClinics = lazy(
-  () => import('../pages/legacy_dashboard/components/organisms/MyClinics')
-);
-const PrescriptionPage = lazy(
-  () =>
-    import('../pages/legacy_dashboard/components/organisms/PrescriptionPage')
-);
-const Statistics = lazy(
-  () => import('../pages/legacy_dashboard/components/organisms/Statistics')
-);
-const EditProfile = lazy(
-  () => import('../pages/legacy_dashboard/components/organisms/EditProfile')
-);
+// const CreateClinic = lazy(
+//   () => import('../pages/setting/components/CreateClinic')
+// );
+// const InviteClinic = lazy(
+//   () => import('../pages/legacy_dashboard/components/organisms/InviteClinic')
+// );
+// const Members = lazy(
+//   () => import('../pages/legacy_dashboard/components/organisms/Members')
+// );
+// const MyClinics = lazy(
+//   () => import('../pages/legacy_dashboard/components/organisms/MyClinics')
+// );
+// const PrescriptionPage = lazy(
+//   () =>
+//     import('../pages/legacy_dashboard/components/organisms/PrescriptionPage')
+// );
+// const Statistics = lazy(
+//   () => import('../pages/legacy_dashboard/components/organisms/Statistics')
+// );
+// const EditProfile = lazy(
+//   () => import('../pages/legacy_dashboard/components/organisms/EditProfile')
+// );
 const Search = lazy(() => import('../pages/search/Search'));
 const Warning = lazy(() => import('../components/Warning'));
 
@@ -106,7 +105,7 @@ const loginRoute = [
       },
     ],
   },
-  { path: 'dashboard/clinic/statistics', element: <Dashboard /> },
+  // { path: 'dashboard/clinic/statistics', element: <Dashboard /> },
   {
     path: 'search',
     element: (
@@ -115,52 +114,52 @@ const loginRoute = [
       </ProtectRoute>
     ),
   },
-  {
-    path: clinics,
-    element: <MyClinics />,
-  },
-  {
-    path: create,
-    element: (
-      <ProtectRoute whenFail={<Warning type="verifyEmail" />} failWhenLogout>
-        <CreateClinic />
-      </ProtectRoute>
-    ),
-  },
-  {
-    path: invite,
-    element: (
-      <ProtectRoute whenFail={<Warning type="verifyEmail" />} failWhenLogout>
-        <InviteClinic />
-      </ProtectRoute>
-    ),
-  },
-  {
-    path: member,
-    element: <Members />,
-  },
-  {
-    path: prescription,
-    element: <PrescriptionPage />,
-    children: [
-      {
-        path: 'create-prescription',
-        element: <CreatePrescription />,
-      },
-    ],
-  },
-  {
-    path: statistics,
-    element: (
-      <ProtectRoute whenFail={<Warning type="verifyEmail" />} failWhenLogout>
-        <Statistics />
-      </ProtectRoute>
-    ),
-  },
-  {
-    path: editProfile,
-    element: <EditProfile />,
-  },
+  // {
+  //   path: clinics,
+  //   element: <MyClinics />,
+  // },
+  // {
+  //   path: create,
+  //   element: (
+  //     <ProtectRoute whenFail={<Warning type="verifyEmail" />} failWhenLogout>
+  //       <CreateClinic />
+  //     </ProtectRoute>
+  //   ),
+  // },
+  // {
+  //   path: invite,
+  //   element: (
+  //     <ProtectRoute whenFail={<Warning type="verifyEmail" />} failWhenLogout>
+  //       <InviteClinic />
+  //     </ProtectRoute>
+  //   ),
+  // },
+  // {
+  //   path: member,
+  //   element: <Members />,
+  // },
+  // {
+  //   path: prescription,
+  //   element: <PrescriptionPage />,
+  //   children: [
+  //     {
+  //       path: 'create-prescription',
+  //       element: <CreatePrescription />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: statistics,
+  //   element: (
+  //     <ProtectRoute whenFail={<Warning type="verifyEmail" />} failWhenLogout>
+  //       <Statistics />
+  //     </ProtectRoute>
+  //   ),
+  // },
+  // {
+  //   path: editProfile,
+  //   element: <EditProfile />,
+  // },
 ];
 
 export default loginRoute;
