463f601761909c7ed914ea233862443225d4fd05
seongjin <ysjkof@gmail.com>
2023-02-09 22:02:08 +0900

    feat: MemberListItem 선택시 바탕색 강조

--- src/pages/dashboard/components/MemberManagement/MemberListItem.tsx
+++ src/pages/dashboard/components/MemberManagement/MemberListItem.tsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { USER_COLORS } from '../../../../constants/constants';
 import { cls, getMemberState } from '../../../../utils/common.utils';
+import { loggedInUserVar } from '../../../../store';
 import type { MemberOfClient } from '../../../../types/common.types';
 
 interface MemberListItemProps {
@@ -15,11 +16,18 @@ const MemberListItem = ({ member, userIndex }: MemberListItemProps) => {
     manager: member.manager,
   });
 
+  const { memberId } = useParams();
+  const enabled = member.id === Number(memberId);
+  const isMe = member.user.id === loggedInUserVar()?.id;
+
   return (
     <li key={member.id}>
       <Link
         to={`${member.id}`}
-        className="flex items-center justify-between gap-4 py-4 px-4 text-base"
+        className={cls(
+          'flex items-center justify-between gap-4 py-4 px-4 text-base',
+          enabled ? 'bg-[#EEEEFF]' : ''
+        )}
       >
         <span
           className="aspect-square w-9 rounded-md bg-red-200 text-center text-white"
