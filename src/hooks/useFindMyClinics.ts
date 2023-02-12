58f5243ae392d298563767d5b4bdeb8552d3baa4
seongjin <ysjkof@gmail.com>
2023-02-12 11:51:54 +0900

    feat: useFindMyClinics이 hasInactivate 매개변수로 받음

--- src/hooks/useFindMyClinics.ts
+++ src/hooks/useFindMyClinics.ts
@@ -6,12 +6,18 @@ import type {
 } from '../types/generated.types';
 import type { ClinicsOfFindMyClinics } from '../types/common.types';
 
-export const useFindMyClinics = (): [
+interface UseFindMyClinicsProps {
+  hasInactivate?: boolean;
+}
+
+export const useFindMyClinics = ({
+  hasInactivate = false,
+}: UseFindMyClinicsProps): [
   ClinicsOfFindMyClinics,
   QueryResult<FindMyClinicsQuery, FindMyClinicsQueryVariables>
 ] => {
   // 비활성은 평소 볼 일이 많지 않기 때문에 필요시에 더 요청해서 보기
-  const variables = { input: { includeInactivate: false } };
+  const variables = { input: { includeInactivate: hasInactivate } };
   const results = useQuery<FindMyClinicsQuery, FindMyClinicsQueryVariables>(
     FIND_MY_CLINICS_DOCUMENT,
     { variables }
