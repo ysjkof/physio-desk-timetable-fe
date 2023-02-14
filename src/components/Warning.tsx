5924dfd44de384aea9f027b784f0f7b29e234c1c
seongjin <ysjkof@gmail.com>
2023-02-14 13:54:45 +0900

    refactor: Warining.tsx의 메시지 객체 수정시 타입 변경되게 함

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -1,12 +1,16 @@
-type WarningType =
-  | 'hasNotPermission'
-  | 'hasNotStatistics'
-  | 'hasNotPrescription'
-  | 'emptyUserIds'
-  | 'emptyData'
-  | 'emptySearch'
-  | 'selectMenu'
-  | 'verifyEmail';
+const messages = {
+  hasNotPermission: '권한이 없습니다',
+  hasNotStatistics: '통계 내역이 없습니다',
+  hasNotPrescription: '처방 내역이 없습니다',
+  emptyUserIds: '사용자를 선택해주세요',
+  emptyData: '데이터가 없습니다',
+  emptySearch: '검색결과가 없습니다',
+  selectMenu: '메뉴를 선택하세요',
+  verifyEmail: '이메일을 인증하면 모든 기능을 사용할 수있습니다',
+  useAfterLoggedOut: '로그아웃 후 사용하세요.',
+};
+
+type WarningType = keyof typeof messages;
 
 interface ChildrenProps {
   children: React.ReactNode;
@@ -18,17 +22,6 @@ interface WarningProps {
 }
 
 const Warning = ({ type, children }: WarningProps | ChildrenProps) => {
-  const messages = {
-    hasNotPermission: '권한이 없습니다',
-    hasNotStatistics: '통계 내역이 없습니다',
-    hasNotPrescription: '처방 내역이 없습니다',
-    emptyUserIds: '사용자를 선택해주세요',
-    emptyData: '데이터가 없습니다',
-    emptySearch: '검색결과가 없습니다',
-    selectMenu: '메뉴를 선택하세요',
-    verifyEmail: '이메일을 인증하면 모든 기능을 사용할 수있습니다',
-  };
-
   return (
     <p className="my-10 mx-auto text-center text-base font-semibold">
       {type ? messages[type] : children}
