7f2c4ad5427e1acab0728681ac467a397af03cda
seongjin <ysjkof@gmail.com>
2023-02-14 10:17:14 +0900

    refactor: Toast.tsx 빠른 null 반환
    가독성 위해 삼항연산자에서 빠른 null반환하기로 변경

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -17,44 +17,44 @@ export default function Toast() {
     setTimeout(closeToast, milliseconds || defaultToastTimeout);
   }, [messages, fade]);
 
+  // TODO: 토스트는 백그라운드 필요 없음. 그냥 사라지는거야. 일정시간뒤에. 개선 고려.
+
+  if (!messages) return null;
+
   return (
     <>
-      {messages && (
-        <>
-          <div
-            className={cls(
-              'position-center fixed z-50 flex flex-col gap-4 rounded-md bg-slate-600 px-6 py-4 text-base text-white',
-              fade ? 'fade animate-fadeout' : ''
-            )}
-            style={{
-              ...(milliseconds && {
-                animationDuration: `${milliseconds / 1000}s`,
-              }),
-            }}
-          >
-            {messages.map((message) => (
-              <p key={message}>{message}</p>
-            ))}
-            <button
-              onClick={closeToast}
-              className="rounded-md border-2 border-white px-1 outline-none focus:border-green-400"
-              type="button"
-            >
-              확인
-            </button>
-          </div>
-          <div
-            className={cls(
-              'fixed top-0 left-0 z-40 h-screen w-screen',
-              bgColor ? 'bg-black/30' : ''
-            )}
-            onClick={closeToast}
-            onKeyDown={closeToast}
-            role="button"
-            tabIndex={0}
-          />
-        </>
-      )}
+      <div
+        className={cls(
+          'position-center fixed z-50 flex flex-col gap-4 rounded-md bg-slate-600 px-6 py-4 text-base text-white',
+          fade ? 'fade animate-fadeout' : ''
+        )}
+        style={{
+          ...(milliseconds && {
+            animationDuration: `${milliseconds / 1000}s`,
+          }),
+        }}
+      >
+        {messages.map((message) => (
+          <p key={message}>{message}</p>
+        ))}
+        <button
+          onClick={closeToast}
+          className="rounded-md border-2 border-white px-1 outline-none focus:border-green-400"
+          type="button"
+        >
+          확인
+        </button>
+      </div>
+      <div
+        className={cls(
+          'fixed top-0 left-0 z-40 h-screen w-screen',
+          bgColor ? 'bg-black/30' : ''
+        )}
+        onClick={closeToast}
+        onKeyDown={closeToast}
+        role="button"
+        tabIndex={0}
+      />
     </>
   );
 }
