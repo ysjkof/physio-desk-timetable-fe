48ecaec31874c9afa4f9e0bdc783b653dcc25f7b
seongjin <ysjkof@gmail.com>
2023-02-25 23:19:23 +0900

    feat: 통계 치료사 검색 구현

--- src/pages/dashboard/components/Statistics/TableChart/TableChart.tsx
+++ src/pages/dashboard/components/Statistics/TableChart/TableChart.tsx
@@ -1,16 +1,33 @@
+import { ChangeEvent, useState } from 'react';
 import TableChartCard from './TableChartCard';
 import { useTableChart } from './useTableChart';
+import { useDebouncedCallback } from '../../../../../hooks';
+import { SearchInput } from '../../../../../components';
 
 const TableChart = () => {
   const { countList } = useTableChart();
+  const [query, setQuery] = useState('');
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setQuery(value);
+  };
+  const debounceQuery = useDebouncedCallback(handleInput);
 
   return (
     <div className="table-chart w-[270px] gap-4">
-      <input className="input" placeholder="치료사 검색" name="searchPatient" />
+      <SearchInput
+        id="table-chart__search-patient"
+        name="table-chart__search-patient"
+        placeholder="치료사 검색"
+        onChange={debounceQuery}
+      />
       <div className="flex w-fit flex-col gap-2">
         {countList &&
           Object.entries(countList).map(([key, value]) => {
-            return <TableChartCard key={key} userId={key} {...value} />;
+            return (
+              <TableChartCard key={key} userId={key} {...value} query={query} />
+            );
           })}
       </div>
     </div>
--- src/pages/dashboard/components/Statistics/TableChart/TableChartCard.tsx
+++ src/pages/dashboard/components/Statistics/TableChart/TableChartCard.tsx
@@ -1,22 +1,31 @@
 import { useGetClinic } from '../../../../../hooks';
-import { PrimaryCountListItem } from '../../../../../types/commonTypes';
+import { cls } from '../../../../../utils/commonUtils';
+import type { PrimaryCountListItem } from '../../../../../types/commonTypes';
 
 interface TableChartCardProps extends PrimaryCountListItem {
   userId: string;
+  query: string;
 }
 
 const TableChartCard = (props: TableChartCardProps) => {
-  const { cancel, newPatient, noshow, reservationCount, userId } = props;
+  const { cancel, newPatient, noshow, reservationCount, userId, query } = props;
   const [myClinic] = useGetClinic();
-  const member = myClinic?.members.find(
-    (member) => member.user.id === Number.parseInt(userId, 10)
-  );
+
+  const name =
+    myClinic?.members.find(
+      (member) => member.user.id === Number.parseInt(userId, 10)
+    )?.user.name || '';
 
   return (
-    <div className="table-chart__card">
+    <div
+      className={cls(
+        'table-chart__card',
+        query && !new RegExp(query).test(name) ? 'hidden' : ''
+      )}
+    >
       <div className="flex items-center gap-1 px-1.5 text-sm font-medium">
         <div className="h-3 w-3 rounded-sm bg-red-300"></div>
-        {member?.user.name}
+        {name}
       </div>
       <div>
         <div className="flex">
