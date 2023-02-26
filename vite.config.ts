3ad9f8edc33bfc4cafe74c1cebfe42f6dfbe3ff8
seongjin <ysjkof@gmail.com>
2023-02-26 10:08:39 +0900

    chore: test 설정(vitest)

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -11,4 +12,8 @@ export default defineConfig({
     visualizer({ gzipSize: true }),
     mdx({ remarkPlugins: [remarkGfm] }),
   ],
+  test: {
+    include: ['test/*'],
+    coverage: { provider: 'c8', reporter: ['html-spa'] },
+  },
 });
