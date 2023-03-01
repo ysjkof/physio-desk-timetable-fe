c9c5553ac4a6a747ae46bf1a54eab58139e3f313
seongjin <ysjkof@gmail.com>
2023-03-01 11:14:28 +0900

    chore: Sentry 설정

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
   readonly PROD: boolean;
   readonly MODE: string;
+  readonly VITE_SENTRY_DNS: string;
 }
 
 interface ImportMeta {
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import router from './router/router';
 import './styles/tailwind.css';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DNS,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
