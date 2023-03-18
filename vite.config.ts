e793bce3bb2249fecd6bc586ab79a2438abafadf
seongjin <77876601+ysjkof@users.noreply.github.com>
2023-03-18 11:57:20 +0900

    chore: vite-plugin-checker 설정
    개발서버에서 실시간으로 타입스크립트 에러 표시 목적

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import mdx from '@mdx-js/rollup';
 import remarkGfm from 'remark-gfm';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     react(),
     visualizer({ gzipSize: true }),
     mdx({ remarkPlugins: [remarkGfm] }),
+    checker({ typescript: true }),
   ],
   test: {
     include: ['tests/*'],
