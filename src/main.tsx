c28ca3b1b76258703b44f55f16cec6692ade4095
seongjin <ysjkof@gmail.com>
2023-04-12 23:45:21 +0900

    chore: 구글분석4 설정

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import router from './router/router';
 import './styles/tailwind.css';
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
+import ga4 from 'react-ga4';
 
 if (import.meta.env.VITE_SENTRY_DNS) {
   Sentry.init({
@@ -12,6 +13,11 @@ if (import.meta.env.VITE_SENTRY_DNS) {
     integrations: [new BrowserTracing()],
     tracesSampleRate: 1,
   });
+}
+
+if (import.meta.env.PROD) {
+  const GA_MEASUREMENT_ID = 'G-QLTJB0LYHW';
+  ga4.initialize(GA_MEASUREMENT_ID);
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
